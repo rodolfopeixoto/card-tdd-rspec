8c2282e17dbfdc4c0c7867fdbaa516ccd75a3cb6
Rodolfo <rodolfog.peixoto@gmail.com>
2016-04-08 12:19:07 -0300

    First commit.

---
+++ lib/card.rb
@@ -0,0 +1,43 @@
+#######################################
+#########
+######### rspec --color --format doc card.rb
+######### rspec --format html --color card.rb > result.html
+#########
+#######################################
+class Card
+
+  def self.build(suit, rank)
+    new(suit: suit, rank: rank)
+  end 
+  private_class_method :new
+
+  def initialize(suit: , rank:)
+    @suit = suit
+    @rank = case rank
+      when :jack  then 11
+      when :queen then 12
+      when :king  then 13
+      else rank
+      end
+    end
+
+  def suit
+    @suit
+  end
+
+  def rank
+   @rank
+  end
+
+  def ==(other)
+    rank == other.rank && suit == other.suit
+  end
+
+  def hash
+    [rank,suit].hash
+  end
+
+  def eql?(other)
+   self == other
+  end
+end
---
+++ spec/cli_spec.rb
@@ -0,0 +1,7 @@
+require 'pty'
+BIN = File.expand_path("../../bin/play",__FILE__)
+describe 'CLI' do
+  example 'it works' do #
+   
+  end
+end
